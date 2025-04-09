233ec234650dc37b14c9fdb6af1840ff568b9afc
jin <jin@hyoo.ru>
2025-04-10 00:40:54 +0300

    clearing

--- password.view.ts
+++ password.view.ts
@@ -6,10 +6,30 @@ namespace $.$$ {
 		}
 		
 		@ $mol_mem
+		fields() {
+			return [
+				this.Master_block(),
+				this.Context_block(),
+				... ( this.master() && this.context() ) ? [ this.Password_block() ] : [],
+			]
+		}
+		
+		@ $mol_mem
 		password() {
 			const context = $mol_charset_encode( this.context() )
 			const sacred = $mol_wire_sync( this.$ ).$mol_crypto_sacred_pass( this.master(), context )
 			return $mol_base64_encode( sacred.asArray() )
+		}
+		
+		context_clear() {
+			this.context( '' )
+			this.Context().focused( true )
+		}
+		
+		close() {
+			this.master( '' )
+			this.context( '' )
+			this.$.$mol_dom.close()
 		}
 		
 	}
