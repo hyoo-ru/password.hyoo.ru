e7f091ea3fc917cf1a6b683db3519948a9096097
nin-jin <sairi-na-tenshi@ya.ru>
2025-04-10 06:30:54 +0000

    Deploying to gh-pages from @ hyoo-ru/password.hyoo.ru@d902a2dfc7cc9ad078f69a7b8007dbc6a24094a5 🚀

--- web.js
+++ web.js
@@ -7499,6 +7499,11 @@ var $;
                     shadow: [[0, 0, $mol_gap.block, 0, $mol_style_func.hsla(0, 0, 0, .2)]],
                 },
             },
+            Password_bar: {
+                justify: {
+                    content: 'space-between',
+                },
+            },
             Copy: {
                 font: {
                     family: 'monospace',
