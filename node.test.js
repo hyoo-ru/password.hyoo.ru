2813d7128f6654b206df8f51fe9bcfa495169f9c
nin-jin <nin-jin@users.noreply.github.com>
2025-10-01 07:17:38 +0000

    Deploying to gh-pages from @ hyoo-ru/password.hyoo.ru@5806f39279b12594a211177c847ef0618dec7760 🚀

--- node.test.js
+++ node.test.js
@@ -5487,6 +5487,9 @@ var $;
 			if(next !== undefined) return next;
 			return null;
 		}
+		activate(next){
+			return (this.event_activate(next));
+		}
 		clicks(next){
 			if(next !== undefined) return next;
 			return null;
@@ -5494,6 +5497,9 @@ var $;
 		event_key_press(next){
 			if(next !== undefined) return next;
 			return null;
+		}
+		key_press(next){
+			return (this.event_key_press(next));
 		}
 		disabled(){
 			return false;
@@ -5524,9 +5530,9 @@ var $;
 		event(){
 			return {
 				...(super.event()), 
-				"click": (next) => (this.event_activate(next)), 
+				"click": (next) => (this.activate(next)), 
 				"dblclick": (next) => (this.clicks(next)), 
-				"keydown": (next) => (this.event_key_press(next))
+				"keydown": (next) => (this.key_press(next))
 			};
 		}
 		attr(){
@@ -5695,7 +5701,7 @@ var $;
             }
             event_key_press(event) {
                 if (event.keyCode === $mol_keyboard_code.enter) {
-                    return this.event_activate(event);
+                    return this.activate(event);
                 }
             }
             tab_index() {
@@ -7615,6 +7621,9 @@ var $;
 		}
 		submit_hint(){
 			return "";
+		}
+		submit_activate(next){
+			return (this.Submit().activate(next));
 		}
 		submit(next){
 			if(next !== undefined) return next;
