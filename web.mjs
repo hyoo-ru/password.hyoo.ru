0cf7bac651a7bb735fe9a9c95d98da31f14fc893
nin-jin <nin-jin@users.noreply.github.com>
2026-04-25 08:04:36 +0000

    Deploying to gh-pages from @ hyoo-ru/password.hyoo.ru@14b3555b81872d12deda1944b6479162aa9d0118 🚀

--- web.mjs
+++ web.mjs
@@ -296,6 +296,8 @@ var $;
         'field',
         'image',
         'spirit',
+        'hue',
+        'hue_spread',
     ]);
 })($ || ($ = {}));
 
@@ -5112,7 +5114,7 @@ var $;
                         }
                     }
                 }
-                else if (key === '@media') {
+                else if (key === '@media' || key === '@container') {
                     const media = config[key];
                     for (let query of Object.keys(media).reverse()) {
                         rules.push('}\n');
